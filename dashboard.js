d98e338113e3e4560af8af11b6a4b353f2dd15c4
jyothi1518 <113339711+jyothi1518@users.noreply.github.com>
2024-04-30 09:49:42 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,53 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, FlatList, Animated } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather'; // Importing Feather icons
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, FlatList, Animated, Dimensions, PixelRatio, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import * as ImagePicker from 'expo-image-picker';
 
-const Dashboard = ({ username, handleLogout, handleImageUpload, handleCalculateBMI, handleCalculateVolume }) => {
+const { width, height } = Dimensions.get('window');
+
+const Dashboard = ({ navigation, handleLogout, handleImageUpload, route }) => {
+  const { username } = route.params;
   const [showSidebar, setShowSidebar] = useState(false);
+  const [showBlueContainer, setShowBlueContainer] = useState(false);
   const scaleValue = useRef(new Animated.Value(1)).current;
+  const blueContainerPosition = useRef(new Animated.Value(height - height * 0.25)).current; // Adjusted initial position based on screen height
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status: cameraStatus } = await ImagePicker.requestCameraPermissionsAsync();
+      const { status: mediaLibraryStatus } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (cameraStatus !== 'granted' || mediaLibraryStatus !== 'granted') {
+        alert('Permission to access camera and photo library required!');
+      }
+    })();
+  }, []);
+
+  const handlePickCamera = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setSelectedImage(result.uri);
+    }
+  };
+
+  const handlePickGallery = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setSelectedImage(result.uri);
+    }
+  };
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
@@ -14,31 +57,42 @@ const Dashboard = ({ username, handleLogout, handleImageUpload, handleCalculateB
     setShowSidebar(false);
   };
 
-  const handleButtonPress = (action) => {
-    Animated.sequence([
-      Animated.timing(scaleValue, {
-        toValue: 0.95,
-        duration: 50,
-        useNativeDriver: true,
-      }),
-      Animated.timing(scaleValue, {
-        toValue: 1,
-        duration: 100,
-        useNativeDriver: true,
-      }),
-    ]).start(() => {
-      // Perform button action
-      action();
-    });
+  const handleLogoutPress = () => {
+    navigation.navigate('LoginScreen');
+    handleLogout();
   };
+
+  const handleDoctorProfilePress = () => {
+    navigation.navigate('DoctorProfileScreen', { username: username });
+  };
+
+  const handleListOfPatientsPress = () => {
+    navigation.navigate('ListOfPatientsScreen');
+  };
+
+  const handleSegregatePress = () => {
+    navigation.navigate('SegregationScreen');
+  };  
+
+  const handleUploadMRIImagesPress = () => {
+    setShowBlueContainer((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    Animated.timing(blueContainerPosition, {
+      toValue: showBlueContainer ? height - height * 0.15 : height - height * 0.13, // Adjusted values for container position
+      duration: 500, // Adjust the duration as needed
+      useNativeDriver: false,
+    }).start();
+  }, [showBlueContainer]);
 
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
         <TouchableOpacity onPress={toggleSidebar}>
-          <Icon name={showSidebar ? 'x' : 'menu'} size={30} color="white" style={styles.menuIcon} />
+          <Icon name={showSidebar ? 'x' : 'menu'} size={30} color="white" style={[styles.menuIcon, styles.sidebarIcon]} />
         </TouchableOpacity>
-        <Text style={styles.title}>Welcome, {username}!</Text>
+        <Text style={styles.title}>Welcome, Doctor!</Text>
       </View>
 
       {showSidebar && (
@@ -52,10 +106,9 @@ const Dashboard = ({ username, handleLogout, handleImageUpload, handleCalculateB
             <Text style={styles.sidebarTitle}>Menu</Text>
             <FlatList
               data={[
-                { title: 'Home', icon: 'home' },
-                { title: 'Doctor Profile', icon: 'user' },
-                { title: 'Patient Profile', icon: 'users' },
-                { title: 'Logout', icon: 'log-out', onPress: handleLogout },
+                { title: 'Doctor Profile', icon: 'user', onPress: handleDoctorProfilePress },
+                { title: 'List of Patients', icon: 'users', onPress: handleListOfPatientsPress },
+                { title: 'Logout', icon: 'log-out', onPress: handleLogoutPress },
               ]}
               renderItem={({ item }) => (
                 <TouchableOpacity onPress={item.onPress}>
@@ -71,27 +124,44 @@ const Dashboard = ({ username, handleLogout, handleImageUpload, handleCalculateB
         </TouchableOpacity>
       )}
 
-      <View style={styles.contentContainer}>
-        <View style={styles.mainContent}>
-          <View style={styles.buttonContainer}>
-            <View style={styles.buttonWrapper}>
-              <Animated.View style={[styles.button, styles.uploadButton, { transform: [{ scale: scaleValue }] }]}>
-                <TouchableOpacity onPress={() => handleButtonPress(handleImageUpload)}>
-                  <Text style={styles.buttonText}>Upload MRI Images</Text>
-                </TouchableOpacity>
-              </Animated.View>
+      <View style={styles.buttonContainer}>
+        {/* Upload MRI images button */}
+        <View style={[styles.buttonWrapper, { marginTop: height * 0.1 }]}>
+          <TouchableOpacity onPress={handleUploadMRIImagesPress}>
+            <View style={[styles.button, styles.uploadMRIButton]}>
+              <Text style={styles.buttonText}>Upload MRI Images</Text>
             </View>
+          </TouchableOpacity>
+        </View>
 
-            <View style={styles.buttonWrapper}>
-              <Animated.View style={[styles.button, styles.calculateButton, { transform: [{ scale: scaleValue }] }]}>
-                <TouchableOpacity onPress={() => handleButtonPress(handleCalculateBMI)}>
-                  <Text style={styles.buttonText}>Segregate using BMI and Volume</Text>
-                </TouchableOpacity>
-              </Animated.View>
+        {/* Segregate button */}
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity onPress={handleSegregatePress}>
+            <View style={[styles.button, styles.segregateButton]}>
+              <Text style={styles.buttonText}>Segregate using BMI and Volume</Text>
             </View>
-          </View>
+          </TouchableOpacity>
         </View>
       </View>
+
+      {showBlueContainer && (
+        <Animated.View style={[styles.blueContainer, { top: blueContainerPosition }]}>
+          <Text style={styles.uploadImageText}>Upload image</Text>
+          <TouchableOpacity onPress={handlePickCamera}>
+            <View style={styles.ellipse}>
+              <Icon name="camera" size={PixelRatio.getPixelSizeForLayoutSize(15)} color="#3D6DCC" />
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.cameraText}>Camera</Text>
+          <TouchableOpacity onPress={handlePickGallery}>
+            <View style={styles.ellipse2}>
+              <Icon name="image" size={PixelRatio.getPixelSizeForLayoutSize(15)} color="#3D6DCC" />
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.galleryText}>Gallery</Text>
+          {selectedImage && <Image source={{ uri: selectedImage }} style={styles.selectedImage} />}
+        </Animated.View>
+      )}
     </View>
   );
 };
@@ -102,53 +172,45 @@ const styles = StyleSheet.create({
     backgroundColor: '#E6EEFF',
   },
   topBar: {
+    width: '100%',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    backgroundColor: '#3D6DCC', // Blue color for the top bar
-    paddingHorizontal: 20,
-    paddingTop: 50, // Adjust as needed for spacing
-    paddingBottom: 20,
-  },
+    backgroundColor: '#3D6DCC',
+    paddingHorizontal: 10,
+    paddingTop: height * 0.1,
+    paddingBottom: height * 0.02,
+  },  
   title: {
-    fontSize: 20,
+    fontSize: width * 0.05, // Adjust the percentage for font size
     color: 'white',
     fontWeight: 'bold',
-  },
+    position: 'absolute',
+    top: height * 0.103, // Adjust the percentage for top position
+    left: '80%',
+    transform: [{ translateX: -width * 0.2 }], // Adjust this value as needed
+},  
   menuIcon: {
     marginRight: 10,
-  },
-  contentContainer: {
-    flex: 1,
-  },
-  mainContent: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    bottom: 0,
+    left: 0,
   },
   buttonContainer: {
-    position: 'relative',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    width: '100%',
+    bottom: height * 0.1,
   },
   buttonWrapper: {
     width: '80%',
-    marginTop: 20,
+    marginBottom: height * 0.03,
   },
   button: {
     width: '100%',
-    paddingVertical: 15,
+    paddingVertical: height * 0.025,
     borderRadius: 25,
     alignItems: 'center',
     justifyContent: 'center',
-    elevation: 3, // Android shadow
-    shadowColor: 'rgba(0, 0, 0, 0.3)', // iOS shadow
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
   },
   buttonText: {
     color: 'white',
@@ -156,16 +218,19 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  uploadButton: {
-    backgroundColor: '#3D6DCC', // Blue color for upload button
+  uploadMRIButton: {
+    backgroundColor: '#3D6DCC',
+    borderRadius: 25,
+    elevation: 20,
   },
-  calculateButton: {
-    backgroundColor: '#3D6DCC', // Red color for calculate button
+  segregateButton: {
+    backgroundColor: '#3D6DCC',
+    borderRadius: 25,
+    elevation: 20,
   },
   sidebarContainer: {
     ...StyleSheet.absoluteFillObject,
     zIndex: 10,
-    elevation: 10, // works on android
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   sidebar: {
@@ -173,25 +238,15 @@ const styles = StyleSheet.create({
     top: 0,
     bottom: 0,
     left: 0,
-    backgroundColor: '#E6EEFF', // Grey background color for the sidebar
-    width: 200,
-    paddingTop: 50,
-    paddingHorizontal: 20,
-    elevation: 4, // Android shadow
-    shadowColor: '#000', // iOS shadow
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    zIndex: 10, // works on ios
-    elevation: 10, // works on android
-
+    backgroundColor: '#E6EEFF',
+    width: width * 0.6,
+    paddingTop: height * 0.1,
+    paddingHorizontal: width * 0.05,
+    zIndex: 10,
   },
   closeButtonContainer: {
     alignItems: 'flex-end',
-    marginBottom: 20,
+    marginBottom: height * 0.03,
   },
   closeButton: {
     alignSelf: 'flex-end',
@@ -199,23 +254,79 @@ const styles = StyleSheet.create({
   sidebarTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: height * 0.02,
     textAlign: 'center',
-    color: '#333', // Dark color for the sidebar title
+    color: '#333',
   },
   menuItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
-    paddingHorizontal: 20,
-  },
-  menuIcon: {
-    marginRight: 10,
+    marginBottom: height * 0.02,
+    paddingHorizontal: width * 0.05,
   },
   menuText: {
-    color: '#333', 
+    color: '#333',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  blueContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: height * 0.25,
+    backgroundColor: 'rgba(61, 109, 204, 1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: width * 0.08,
+    top: height * 0.2,
+  },
+  uploadImageText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#FFFFFF',
+    position: 'absolute',
+    top: height * 0.02,
+    left: width * 0.05,
+  },
+  ellipse: {
+    position: 'absolute',
+    width: 66,
+    height: 59,
+    borderRadius: 50,
+    backgroundColor: 'rgba(230, 238, 255, 1)',
+    bottom: - height * 0.06, 
+    right: width * 0.12,
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+  ellipse2: {
+    width: 66,
+    height: 59,
+    borderRadius: 50,
+    backgroundColor: 'rgba(230, 238, 255, 1)',
+    bottom: height * 0.015,
+    left: width * 0.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraText: {
+    fontSize: 14,
+    color: '#FFFFFF',
+    position: 'absolute',
+    bottom: height * 0.07,
+    left: width * 0.22,
+  },
+  galleryText: {
+    fontSize: 14,
+    color: '#FFFFFF',
+    position: 'absolute',
+    bottom: height * 0.068,
+    right: width * 0.19,
+  },
+  selectedImage: {
+    width: width * 0.3,
+    height: width * 0.3,
+    borderRadius: width * 0.15,
+    marginVertical: height * 0.025,
   },
 });
 
